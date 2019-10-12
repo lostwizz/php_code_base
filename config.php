e41612a079b11c37a7ce3744c88d6db5396381a2
lostwizz <lostwizz@hotmail.com>
2019-10-11 22:44:35 -0700

    test 7

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
+
+#define DATE_NOW       $WCNOW$
+#define DATE_COMMIT    $WCDATE$
+#define DATE           "$WCUNVER?DATE_NOW:DATE_COMMIT$"
+// $Id: xx.c 1.3.bxx.2.byy.1 30e82ef 2016-04-03 14:48:24 +1000 Joe Blow $
+
 $database = array(
     "driver"   => "mysql",
     "host"     => "locahost",
